ab05c929808084127bf8801be7641669e7f2b81b
Michael <koshak@gmx.at>
2015-09-09 15:39:46 +0200

    added squirrel images, themes and styles

--- app/src/main/java/com/ko/peerzone_android_client/MainActivity.java
+++ app/src/main/java/com/ko/peerzone_android_client/MainActivity.java
@@ -4,13 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton FAB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FAB = (ImageButton) findViewById(R.id.imageButton);
+        FAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "baka!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
